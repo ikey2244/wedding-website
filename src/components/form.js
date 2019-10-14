1b68edd3d72d97c0dd1fe96e736be73f7f0107b9
Isaac Weber <ikey2244@gmail.com>
2019-10-13 19:50:47 -0500

    Add form stuff

--- src/components/form.js
+++ src/components/form.js
@@ -29,14 +29,6 @@ export default function Form() {
       .catch((error) => alert(error))
   }
 
-  function selectOnlyThis(id){
-    var myCheckbox = document.getElementsByName("myCheckbox");
-    Array.prototype.forEach.call(myCheckbox,function(el){
-      el.checked = false;
-    });
-    id.checked = true;
-  }
-
   return (
     <div className="max-w-lg mx-auto">
       <form 
@@ -62,13 +54,13 @@ export default function Form() {
         </div>
 
         <div className="mb-4">
-          <label className="block font-body text-gray-700 text-sm font-bold mb-2" for="grid-state">
+          <label className="block font-body text-gray-700 text-sm font-bold mb-2" htmlFor="response">
             Response 
           </label>
           <div className="relative">
-            <select required name="response" onChange={handleChange} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white" id="grid-state">
-              <option name="accept">Accept</option>
-              <option name="decline">Decline</option>
+            <select name="response" onChange={handleChange} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white" >
+              <option value="accept" defaultValue="accept">Accept</option>
+              <option value="decline">Decline</option>
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
