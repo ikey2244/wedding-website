213d5a0aa927cff07817bc19e95edafe16b1311f
Isaac Weber <ikey2244@gmail.com>
2019-10-13 19:12:35 -0500

    Test

--- src/components/form.js
+++ src/components/form.js
@@ -53,12 +53,12 @@ export default function Form() {
 
         <div className="mb-4">
           <label className="block font-body text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">Full Name</label>
-          <input required style={{fontFamily: 'poppins'}} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required type="text" id="fullName" placeholder="John Smith" name="fullName" onChange={handleChange} />
+          <input style={{fontFamily: 'poppins'}} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required type="text" id="fullName" placeholder="John Smith" name="fullName" onChange={handleChange} />
         </div>
 
         <div className="mb-4">
           <label className="block font-body text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-          <input required style={{fontFamily: 'poppins'}} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required type="email" id="email" placeholder="example@example.com" name="email" onChange={handleChange}  />
+          <input  style={{fontFamily: 'poppins'}} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required type="email" id="email" placeholder="example@example.com" name="email" onChange={handleChange}  />
         </div>
 
         <div className="mb-4">
@@ -67,8 +67,8 @@ export default function Form() {
           </label>
           <div className="relative">
             <select required name="response" onChange={handleChange} className="text-body shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white" id="grid-state">
-              <option value="accept">Accept</option>
-              <option value="decline">Decline</option>
+              <option name="accept">Accept</option>
+              <option name="decline">Decline</option>
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
